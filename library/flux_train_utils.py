de71cba850a3e4ce5a86780899748ed23870cd6b
SlZeroth <qkrwncks593@gmail.com>
2025-04-28 10:28:16 +0900

    w

--- library/flux_train_utils.py
+++ library/flux_train_utils.py
@@ -436,8 +436,14 @@ def get_noisy_model_input_and_timesteps(
         else:
             sigmas = torch.rand((bsz,), device=device)
         # TODO : current_shift 를 이용해서 shift 해야함
-        sigmas_shifted = sigmas * (1.0 - current_shift) + current_shift
-        timesteps = sigmas_shifted * num_timesteps
+        shifted = sigmas * (1.0 - current_shift) + current_shift
+        L = noise_scheduler.timesteps.numel()
+        indices = (shifted * (L - 1)).long().clamp(0, L - 1)
+        timesteps = noise_scheduler.timesteps[indices].to(device=device, dtype=dtype)
+        scheduler_sigmas = noise_scheduler.sigmas[indices].to(device=device, dtype=dtype)
+        sigmas = scheduler_sigmas.view(-1, 1, 1, 1)
+        
+        # timesteps = sigmas_shifted * num_timesteps
         # timesteps = sigmas * num_timesteps
     elif args.timestep_sampling == "shift":
         shift = args.discrete_flow_shift
